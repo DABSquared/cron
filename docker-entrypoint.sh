eff6a8e0d77056238591c6e5ecb025fa331df305
Daniel Brooks <dbrooks@dabsquared.com>
2017-08-08 11:38:49 -0700

    Working on cron image

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,9 @@ set -e
 
 if [ "$CRONTAB_CONF" ]; then
     cp $CRONTAB_CONF /etc/cron.d/symfony
+    sed -i.bak 's/symfony_app_name/'"$symfony_app_name"'/g' /etc/cron.d/symfony
+    sed -i.bak 's/ENVIRONMENT/'"$ENVIRONMENT"'/g' /etc/cron.d/symfony
+    rm -rf /etc/cron.d/symfony.bak
     chmod 0644 /etc/cron.d/symfony
 fi
 
