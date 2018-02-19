e49fc59843c2a592307d3ab965fc07cb4c8c2b09
Daniel <bassrock@users.noreply.github.com>
2018-02-18 17:22:55 -0800

    Updating docker

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,28 +1,10 @@
 #!/bin/bash
 set -e
 
-if [[ -z "$GIT_REPO" && -z "$GIT_SSH_KEY" ]]
-then
-    echo "No GIT Repository defined, not pulling."
-    if [ "$WAIT_FOR_PHP" == "true" ]; then
-        while true
-        do
-          [ -f .php_setup ] && break
-          sleep 2
-        done
-    fi
-else
-    echo "Pulling GIT Repository to /var/www/symfony"
-    mkdir -p ~/.ssh
-    eval "$(ssh-agent)" && ssh-agent -s
-    echo "$GIT_SSH_KEY" > ~/.ssh/id_rsa
-    chmod -R 0600 ~/.ssh/id_rsa
-    ssh-add ~/.ssh/id_rsa
-    [[ -f /.dockerenv ]] && echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
-    cd /var/www
-    git clone --depth 1 "$GIT_REPO" symfony
-    /setup.sh
-fi
+	if [ "$ISDEV" == "false" ]; then
+		php -d newrelic.appname="$symfony_app_name" bin/console --env="$ENVIRONMENT" doctrine:migrations:migrate --no-interaction || (echo >&2 "Doctrine Migrations Failed" && exit 1)
+		php -d newrelic.appname="$symfony_app_name" bin/console --env="$ENVIRONMENT" cache:warmup || (echo >&2 "Cache Warmup Dev Failed" && exit 1)
+	fi
 
 if [ "$CRONTAB_CONF" ]; then
     cp /var/www/symfony/$CRONTAB_CONF /etc/cron.d/symfony
