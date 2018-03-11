54196b8129823c80990f8ced457797b21284e2db
Daniel <bassrock@users.noreply.github.com>
2018-03-11 13:13:17 -0700

    Adding in project env

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-	if [ "$ISDEV" == "false" ]; then
-		php -d newrelic.appname="$symfony_app_name" bin/console --env="$ENVIRONMENT" doctrine:migrations:migrate --no-interaction || (echo >&2 "Doctrine Migrations Failed" && exit 1)
-		php -d newrelic.appname="$symfony_app_name" bin/console --env="$ENVIRONMENT" cache:warmup || (echo >&2 "Cache Warmup Dev Failed" && exit 1)
-	fi
+if [ "$ISDEV" == "false" ]; then
+    php -d newrelic.appname="$symfony_app_name" bin/console --env="$ENVIRONMENT" doctrine:migrations:migrate --no-interaction || (echo >&2 "Doctrine Migrations Failed" && exit 1)
+    php -d newrelic.appname="$symfony_app_name" bin/console --env="$ENVIRONMENT" cache:warmup || (echo >&2 "Cache Warmup Dev Failed" && exit 1)
+fi
 
 if [ "$CRONTAB_CONF" ]; then
     cp /var/www/symfony/$CRONTAB_CONF /etc/cron.d/symfony
@@ -13,5 +13,8 @@ if [ "$CRONTAB_CONF" ]; then
     rm -rf /etc/cron.d/symfony.bak
     chmod 0644 /etc/cron.d/symfony
 fi
+
+printenv | sed 's/^\(.*\)$/export "\1"/g' > /root/project_env.sh
+chmod a+x /root/project_env.sh
 
 exec "$@"
